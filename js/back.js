2ce8abe312a9927d09163ea7d52af0ebd5a6fddf
Shizbriz <56767854+Shizbriz@users.noreply.github.com>
2021-09-02 18:30:28 +0100

    third commit

--- js/back.js
+++ js/back.js
@@ -1,16 +1,16 @@
 function createBubble (){
     const section = document.getElementById("home")
     const createElement = document.createElement('span')
-    let size = Math.random()*5;
+    let size = Math.random()*10;
     createElement.style.width = size + 'px';
     createElement.style.height = size + "px";
     createElement.style.left = Math.random() * innerWidth + "px";
     section.appendChild(createElement); 
 
-    
+    setTimeout(()=>{createElement.remove()}, 60000)
 }
 
-setInterval(createBubble, 200)
+setInterval(createBubble, 300)
 
 
 
